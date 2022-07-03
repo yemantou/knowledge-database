1480575df848b0a6c6454431a8940a4183316858
huangyong <huangyong@talkweb.com.cn>
2022-07-03 20:29:35 +0800

    Promise

--- docs/web-learning/js/test.js
+++ docs/web-learning/js/test.js
@@ -1,4 +1,13 @@
-// A
-ajax('..', function() {
+// 不要只是直接这样使用（假设工具为tool(..)）
+tool(22).then(
+  function(v) {
+    console.log(v);
+  }
+);
 
-})
+// 应该这样使用
+Promise.resolve(tool(22)).then(
+  function(v) {
+    console.log(v);
+  }
+);
